0209b7921f8ac57566ffa425028ed7bcb5605b9b
joshiad <joshiad@dickinson.edu>
2018-10-14 20:12:24 -0400

    Update ABOthelloPlayer.java

--- src/ABOthelloPlayer.java
+++ src/ABOthelloPlayer.java
@@ -1,24 +1,49 @@
 import java.util.Date;
 import java.util.AbstractSet;
 
+/** 
+ * @author Catalina Ionescu, Aditi Joshi
+ *
+ * A class for our own OthelloPlayer
+ * This player uses minimax algorithm with alpha beta pruning
+ * to choose the best move for the MaxPlayer.
+ */
 public class ABOthelloPlayer extends OthelloPlayer implements MiniMax{
-
+	
+	// Based on the experiment result depthLimit is set to 4. 
     private int depthLimit = 4;
     private static int staticEvaluations = 0;
     private static int totalSuccessors = 0;
     private static int exploredSuccessors = 0;
     private static int totalParents = 0;
 
+    /**
+     * Constructor 1
+     * 
+     * @param name the name of the player
+     */
     public ABOthelloPlayer (String name) {
         super(name);
     }
 
+    /**
+     * Constructor 2
+     * 
+     * @param name the name of the player
+     * @param depthLimit maximum depth that can be explored
+     */
     public ABOthelloPlayer (String name, int depthLimit) {
         super(name);
         this.depthLimit = depthLimit;
     }
 
-    //the get move should implement minimaxAlgorithm
+    /**
+     * This method uses alpha beta pruning to find the best move for MaxPlayer
+     * 
+     * @param currentState current state of the game
+     * @param deadline maximum amount of time the operation can take
+     * @return return the best move for MaxPlayer
+     */
     @Override
     public Square getMove(GameState currentState, Date deadline) {
         AbstractSet<GameState> successors = currentState.getSuccessors(true);
@@ -28,6 +53,7 @@ public class ABOthelloPlayer extends OthelloPlayer implements MiniMax{
 
         int evaluation = Integer.MAX_VALUE;
 
+        // Minimax with alpha beta pruning
         for (GameState state : successors) {
             int curEval = minValue(state, Integer.MIN_VALUE, Integer.MAX_VALUE, 1);
 
@@ -43,6 +69,13 @@ public class ABOthelloPlayer extends OthelloPlayer implements MiniMax{
 
     }
 
+    /**
+     * Check if the state is terminal, or if the depth limit has been exceeded
+     * 
+     * @param state the state to be evaluated
+     * @param depth current depth of the state
+     * @return true if it terminal state, else return false;
+     */
     private boolean isTerminalState(GameState state, int depth) {
 
         if (depthLimit != -1 && depth >= depthLimit) return true;
@@ -53,6 +86,15 @@ public class ABOthelloPlayer extends OthelloPlayer implements MiniMax{
 
     }
 
+    /**
+     * It maximizes the value of the evaluation function.
+     * 
+     * @param state the state to be evaluated
+     * @param a value of the best alternative for max
+     * @param b value of the best alternative for min
+     * @param depth current depth of the state
+     * @return the maximum value of the evaluation function
+     */
     public int maxValue(GameState state, int a, int b, int depth) {
         if (isTerminalState(state, depth)) {
             return staticEvaluator(state);
@@ -79,6 +121,16 @@ public class ABOthelloPlayer extends OthelloPlayer implements MiniMax{
 
     }
 
+    /**
+     * It minimizes the value of the evaluation function.
+     * 
+     * @param state the state to be evaluated
+     * @param a value of the best alternative for max
+     * @param b value of the best alternative for min
+     * @param depth current depth of the state
+     * @return the minimum value of the evaluation function 
+     */
+    
     public int minValue(GameState state, int a, int b, int depth) {
         if (isTerminalState(state, depth)) {
             return staticEvaluator(state);
@@ -99,27 +151,56 @@ public class ABOthelloPlayer extends OthelloPlayer implements MiniMax{
         return v;
 
     }
-
+    
+    /**
+     * Compute the value of the simple static evaluation function
+     * 
+     * @state the state to be evaluated
+     * @return the value of the simple static evaluation function
+     */
     @Override
     public int staticEvaluator(GameState state) {
         staticEvaluations++;
         return state.getScore(state.getCurrentPlayer());
     }
 
+    /**
+     * Get the number of nodes generated
+     * 
+     * @return the number of nodes generated.
+     */
     @Override
     public int getNodesGenerated() {
         return exploredSuccessors;
     }
 
+    /**
+     * Get the number of static evaluations
+     * 
+     * @return the number of static evaluations performed.
+     */
     @Override
     public int getStaticEvaluations() {
         return staticEvaluations;
     }
 
+    /**
+     * Get the average branching factor of the nodes that
+     * were expanded during the search.
+     * 
+     * @return the average branching factor.
+     */
     @Override
     public double getAveBranchingFactor() {
         return (double)totalSuccessors/(double)totalParents;
     }
+    
+    /**
+     * Get the effective branching factor of the nodes that
+     * were expanded during the search.
+     * 
+     * @return the effective branching factor.
+     */
     @Override
     public double getEffectiveBranchingFactor() {
         return (double)exploredSuccessors/(double)totalParents;
